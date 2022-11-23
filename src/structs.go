c1936b449df816d051f2dba81682cef551a68b29
YABOI <110062350+YABOIpy@users.noreply.github.com>
2022-11-23 15:43:40 +0100

    Create structs.go

---
+++ src/structs.go
@@ -0,0 +1,20 @@
+package source
+
+import (
+	"net/http"
+)
+
+type Checker struct {
+	Client http.Client
+	Invalid		int
+	Locked		int
+	Token 		string
+	Valid		int
+	Resp		bool
+	All			int
+}
+
+var (
+	url = "https://discord.com/api/v9/users/@me/affinities/guilds"
+	c, r, g, bg, rb, gr, u =  "\u001b[30;1m", "\033[39m", "\033[32m", "\u001b[34;1m", "\u001b[0m", "\u001b[30;1m", "\u001b[4m"
+)
